82f78d1dcd4de621decf2faa71fd56bff17ee946
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2024-02-07 13:54:34 +0000

    Code the Gadget and Mobile class done

--- TheGadgetShopJavaCoursework/src/thegadgetshopjavacoursework/Models/Gadged.java
+++ TheGadgetShopJavaCoursework/src/thegadgetshopjavacoursework/Models/Gadged.java
@@ -5,5 +5,71 @@ package thegadgetshopjavacoursework.Models;
  */
 public class Gadged
 {
+    // INstance Variables
+    
+    private String model;
+    private double price;
+    private int weight;
+    private String size;
+
+    public Gadged()
+    {
+    }
+
+    public Gadged(String model, double price, int weight, String size)
+    {
+        this.model = model;
+        this.price = price;
+        this.weight = weight;
+        this.size = size;
+    }
+
+    public String getSize()
+    {
+        return size;
+    }
+
+    public void setSize(String size)
+    {
+        this.size = size;
+    }
+
+    public String getModel()
+    {
+        return model;
+    }
+
+    public void setModel(String model)
+    {
+        this.model = model;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+
+    public int getWeight()
+    {
+        return weight;
+    }
+
+    public void setWeight(int weight)
+    {
+        this.weight = weight;
+    }
+    
+    public  void display()
+    {
+        System.out.println("Model: " + model);
+        System.out.println("Price: " + price + " Pounds");
+        System.out.println("Weight: " + weight + " Grams");
+        System.out.println("Size " + size);
+    }
     
 }
--- TheGadgetShopJavaCoursework/src/thegadgetshopjavacoursework/Models/Mobile.java
+++ TheGadgetShopJavaCoursework/src/thegadgetshopjavacoursework/Models/Mobile.java
@@ -1,9 +1,69 @@
 package thegadgetshopjavacoursework.Models;
+
 /**
  *
  * @author Emilio
  */
-public class Mobile
+public class Mobile extends Gadged
 {
+
+    private int numberOfMinutesOfCallingCreditRemaining;
+
+    public Mobile()
+    {
+    }
+
+    public Mobile(int numberOfMinutesOfCallingCreditRemaining)
+    {
+        this.numberOfMinutesOfCallingCreditRemaining = numberOfMinutesOfCallingCreditRemaining;
+    }
+
+    public Mobile(int numberOfMinutesOfCallingCreditRemaining, String model, double price, int weight, String size)
+    {
+        super(model, price, weight, size);
+        this.numberOfMinutesOfCallingCreditRemaining = numberOfMinutesOfCallingCreditRemaining;
+    }
+
+    public int getNumberOfMinutesOfCallingCreditRemaining()
+    {
+        return numberOfMinutesOfCallingCreditRemaining;
+    }
+
+    public void setNumberOfMinutesOfCallingCreditRemaining(int numberOfMinutesOfCallingCreditRemaining)
+    {
+        this.numberOfMinutesOfCallingCreditRemaining = numberOfMinutesOfCallingCreditRemaining;
+    }
+
+    public void addCallingCredit(int credit)
+    {
+        if (credit > 0)
+        {
+            numberOfMinutesOfCallingCreditRemaining += credit;
+        }
+        else
+        {
+            System.out.println("Please enter a positive amount for adding credit.");
+        }
+    }
+
+    public void mobileMakeCall(String phoneNumber, int duration)
+    {
+        if (numberOfMinutesOfCallingCreditRemaining >= duration)
+        {
+            System.out.println("Calling " + phoneNumber + " for " + duration + " minutes.");
+            numberOfMinutesOfCallingCreditRemaining -= duration;
+        }
+        else
+        {
+            System.out.println("Insufficient credit to make the call.");
+        }            
+    }
+    
+      @Override
+        public void display()
+        {
+            super.display();
+            System.out.println("Calling Credit: " + numberOfMinutesOfCallingCreditRemaining + " minutes");
+        }
     
 }
