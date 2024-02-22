6e936c327dde9149b7e0302b0641de3b7a7e6f27
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2024-02-22 15:59:42 +0000

    Documentations Mobile Class

--- TheGadgetShopJavaCoursework/src/thegadgetshopjavacoursework/Models/Mobile.java
+++ TheGadgetShopJavaCoursework/src/thegadgetshopjavacoursework/Models/Mobile.java
@@ -1,37 +1,61 @@
 package thegadgetshopjavacoursework.Models;
 
 /**
+ * Subclass Mobile inheriting from Gadget. Represents a mobile device with
+ * additional functionality such as calling credit management. Extends the
+ * functionality of the Gadget class.
+ *
+ * Mobile objects are used to represent mobile devices within the Gadget Shop
+ * application. They inherit attributes and methods from the Gadget class.
  *
  * @author Emilio
  */
-// Subclass Mobile inheriting from Gadget
 public class Mobile extends Gadget
 {
 
-    private int numberOfMinutesOfCallingCreditRemaining;
+    private int numberOfMinutesOfCallingCreditRemaining; // Number of calling credit minutes remaining
 
-    // Constructor empty
+    // Constructors
+    /**
+     * Default constructor for creating a Mobile object. Initializes the object
+     * with default values.
+     */
     public Mobile()
     {
 
     }
 
-    // Constructor to initialize Mobile attribute and Gatged attributes
+    /**
+     * Constructor for initializing Mobile attributes and Gadget attributes with
+     * specified values.
+     *
+     * @param numberOfMinutesOfCallingCreditRemaining Number of calling credit
+     * minutes remaining
+     * @param gadgetId Unique ID of the mobile gadget
+     * @param model Model of the mobile gadget
+     * @param price Price of the mobile gadget
+     * @param weight Weight of the mobile gadget
+     * @param size Size of the mobile gadget
+     */
     public Mobile(int numberOfMinutesOfCallingCreditRemaining, int gadgetId, String model, double price, int weight, String size)
     {
         super(gadgetId, model, price, weight, size);
-
         this.numberOfMinutesOfCallingCreditRemaining = numberOfMinutesOfCallingCreditRemaining;
     }
 
-    // Constructor to initialize Mobile attribute
+    /**
+     * Constructor for initializing only Mobile attributes with specified
+     * values.
+     *
+     * @param numberOfMinutesOfCallingCreditRemaining Number of calling credit
+     * minutes remaining
+     */
     public Mobile(int numberOfMinutesOfCallingCreditRemaining)
     {
-
         this.numberOfMinutesOfCallingCreditRemaining = numberOfMinutesOfCallingCreditRemaining;
     }
 
-    // Accesor mehtod for numberOfMinutesOfCallingCreditRemaining
+    // Accessor method for numberOfMinutesOfCallingCreditRemaining
     public int getNumberOfMinutesOfCallingCreditRemaining()
     {
         return numberOfMinutesOfCallingCreditRemaining;
@@ -47,19 +71,15 @@ public class Mobile extends Gadget
         {
             System.out.println("");
             System.out.println("***** - Sorry!. Please enter a positive amount for adding credit. - *****");
-            // this.numberOfMinutesOfCallingCreditRemaining = 0;
-
         }
-
     }
 
-    // Mehtod to add calling credit
+    // Method to add calling credit
     public void addCallingCredit(int credit)
     {
         if (credit >= 0)
         {
             numberOfMinutesOfCallingCreditRemaining += credit;
-
         }
         else
         {
@@ -68,10 +88,9 @@ public class Mobile extends Gadget
         }
     }
 
-    // Mehtod to make a call
+    // Method to make a call
     public boolean mobileMakeCall(int phoneNumber, int duration)
     {
-
         if (numberOfMinutesOfCallingCreditRemaining != 0 && numberOfMinutesOfCallingCreditRemaining >= duration)
         {
             System.out.println("CALLING NUMBER: " + phoneNumber + " FOR " + duration + " MINUTES.");
@@ -80,12 +99,11 @@ public class Mobile extends Gadget
         }
         else
         {
-            // System.out.println("*****  - Sorry!. Insufficient credit to make the call.  - *****");
             return false;
         }
     }
 
-    // Override display mehtod to include calling credit
+    // Override display method to include calling credit
     @Override
     public String display()
     {
