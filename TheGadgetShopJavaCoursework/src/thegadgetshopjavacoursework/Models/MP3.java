480a6706ad3f27da741257144d68d0c4c77dd574
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2024-02-22 16:20:58 +0000

    Documentation MP3 class Code

--- TheGadgetShopJavaCoursework/src/thegadgetshopjavacoursework/Models/MP3.java
+++ TheGadgetShopJavaCoursework/src/thegadgetshopjavacoursework/Models/MP3.java
@@ -1,34 +1,56 @@
 package thegadgetshopjavacoursework.Models;
 
 /**
+ * Subclass MP3 inheriting from Gadget. Represents an MP3 player gadget with
+ * additional functionality such as music management. Extends the functionality
+ * of the Gadget class.
+ *
+ * MP3 objects are used to represent MP3 players within the Gadget Shop
+ * application. They inherit attributes and methods from the Gadget class.
  *
  * @author Emilio
  */
-//  Subclass MP£ inheriting from Gadget
 public class MP3 extends Gadget
 {
 
-    //   private int mp3Id;
-    private double availableMemory;
+    private double availableMemory; // Available memory in the MP3 player
 
-    // Constructor empty
+    // Constructors
+    /**
+     * Default constructor for creating an MP3 object. Initializes the object
+     * with default values.
+     */
     public MP3()
     {
-            
     }
 
-    // Contructor to initialize MP3 atributte
+    /**
+     * Constructor for initializing only the available memory attribute of the
+     * MP3 player.
+     *
+     * @param availableMemory Available memory of the MP3 player in megabytes
+     * (MB)
+     */
     public MP3(double availableMemory)
     {
-
         this.availableMemory = availableMemory;
     }
 
-    // Contructor to initialize MP3 atributte and Gatget attributes
+    /**
+     * Constructor for initializing both MP3 attributes and Gadget attributes
+     * with specified values.
+     *
+     * @param availableMemory Available memory of the MP3 player in megabytes
+     * (MB)
+     * @param gadgetId Unique ID of the MP3 gadget
+     * @param model Model of the MP3 gadget
+     * @param price Price of the MP3 gadget
+     * @param weight Weight of the MP3 gadget
+     * @param size Size of the MP3 gadget
+     */
     public MP3(double availableMemory, int gadgetId, String model, double price, int weight, String size)
     {
         super(gadgetId, model, price, weight, size);
-
         this.availableMemory = availableMemory;
     }
 
@@ -43,20 +65,31 @@ public class MP3 extends Gadget
         return this.availableMemory = availableMemory;
     }
 
-    // Method to dowload music
-    public boolean downloadMusic(double memoryReque)
+    // Method to download music
+    /**
+     * Downloads music to the MP3 player.
+     *
+     * @param memoryRequired Memory required for downloading the music in
+     * megabytes (MB)
+     * @return true if the music is successfully downloaded, false otherwise
+     */
+    public boolean downloadMusic(double memoryRequired)
     {
-        if (memoryReque <= availableMemory)
+        if (memoryRequired <= availableMemory)
         {
-            availableMemory -= memoryReque;
+            availableMemory -= memoryRequired;
             return true;
         }
-
         return false;
-
     }
 
-    // Method to delete music (More MB for the MP£ Display)
+    // Method to delete music (Free up memory in the MP3 player)
+    /**
+     * Deletes music from the MP3 player, freeing up memory.
+     *
+     * @param memoryFreed Memory freed by deleting music in megabytes (MB)
+     * @return true if the music is successfully deleted, false otherwise
+     */
     public boolean deleteMusic(int memoryFreed)
     {
         if (memoryFreed >= 0)
@@ -71,14 +104,17 @@ public class MP3 extends Gadget
     }
 
     // Override display method to include available memory
+    /**
+     * Generates a string representation of the MP3 player, including its
+     * details and available memory.
+     *
+     * @return A string containing details of the MP3 player (ID, model, price,
+     * weight, size) and available memory
+     */
     @Override
     public String display()
     {
-          return super.display() + "\n " + "AVAILABLE MEMORY: " + availableMemory  + " MB";
-        //System.out.println("AVAILABLE MEMORY: " + availableMemory + " MB");
+        return super.display() + "\n " + "AVAILABLE MEMORY: " + availableMemory + " MB";
     }
 
-  
-    
-    
 }
